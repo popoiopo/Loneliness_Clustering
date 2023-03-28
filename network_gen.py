f79714f219d7f485907b924d0b176191a3975fd6
Bas Chatel <bastiaan.chatel@gmail.com>
2023-03-28 10:55:09 +0200

    Debugged network generation system

--- network_gen.py
+++ network_gen.py
@@ -13,11 +13,11 @@ import sys
 import os
 
 
-def main(conf_file, n_networks):
+def main(conf_file, n_networks, output_folder):
     # Get network configuration info
     with open(conf_file, "r") as f:
         conf = json.load(f)
-    conf_path = f"graphs/{conf['network_gen_fn']}-{conf['e_samples']}es-{conf['n_per_group']}n-{conf['p_rel']}p"
+    conf_path = f"{output_folder}/{conf['network_gen_fn']}-{conf['e_samples']}es-{conf['n_per_group']}n-{conf['p_rel']}p"
     if not os.path.exists(conf_path):
         os.makedirs(conf_path)
 
@@ -39,12 +39,12 @@ def main(conf_file, n_networks):
 
 
 if __name__ == "__main__":
-    if (args_count := len(sys.argv)) > 3:
+    if (args_count := len(sys.argv)) > 4:
         print(f"Two arguments expected, got {args_count - 1}.")
         raise SystemExit(2)
-    elif args_count < 2:
+    elif args_count < 3:
         print(
-            "You must specify the configuration file and the number of networks you want to generate."
+            "You must specify the configuration file, the number of networks you want to generate, and the root output folder."
         )
         raise SystemExit(2)
 
@@ -52,9 +52,14 @@ if __name__ == "__main__":
     # there are to be generated.
     conf_file = Path(os.path.join("input/configs/", sys.argv[1]))
     n_networks = int(sys.argv[2])
+    output_folder = Path(sys.argv[3])
 
     if not os.path.exists(conf_file):
         print(f"Given configuration file does not exits: {conf_file}")
         raise SystemExit(2)
 
-    main(conf_file, n_networks)
+    if not os.path.exists(output_folder):
+        print(f"Given output folder does not exits: {output_folder}")
+        raise SystemExit(2)
+
+    main(conf_file, n_networks, output_folder)
